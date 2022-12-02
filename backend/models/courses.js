d0ae713947121753070b1771ff9b9fbbd6a52a1f
Adithya S K <adithyaskolavi@gmail.com>
2022-12-02 08:48:56 +0530

    course data and schema updated - Adithya S K

--- backend/models/courses.js
+++ backend/models/courses.js
@@ -2,10 +2,14 @@ const mongoose = require('mongoose');
 
 const coursesSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  img: { type: String, required: true },
-  year: { type: Number, required: true },
-  genre: { type: [String], required: true },
+  price: { type: Number, required: true },
+  website: { type: [String], required: true },
+  company: { type: [String], required: true },
+  certificate: { type: [String], required: true },
   rating: { type: Number, required: true },
+  hours: { type: Number, required: true },
+  video: { type: String, required: false },
+  link: { type: String, required: true },
 });
 
 module.exports = mongoose.model('courses', coursesSchema);
