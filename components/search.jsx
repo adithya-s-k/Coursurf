557b69df77fa46d7f5cd51d85632de7bb1d30b58
SarangKumar <sarangkumar1578@gmail.com>
2023-02-04 21:20:03 +0530

    added submit on enter functionality

--- components/search.jsx
+++ components/search.jsx
@@ -5,19 +5,12 @@ import { RxCross2 } from 'react-icons/rx'
 import Link from 'next/link';
 
 
-
-
 function Search() {
 
     useEffect(() => {
         const input = document.querySelector('input');
+        // const form = document.querySelector('form');
         input.focus();
-        input.addEventListener('keydown', (event) => {
-            if (event.key === 'Enter') {
-                event.preventDefault();
-                document.querySelector("button").click();
-            }
-        })
     }, [])
 
     const clear = () => {
@@ -26,23 +19,26 @@ function Search() {
             inputRef.current.value = '';
         }
     }
+
     const [query, setQuery] = useState('');
 
     const inputRef = useRef('')
     return (
         <div className={search.center}>
-            <div className={search.searchLocal}>
+            <form className={search.searchLocal} action={`/details/${query.split(' ').join('-')}`}>
                 <Link href={`/details/${query.split(' ').join('-')}`} className={search.icon}>
                     <AiOutlineSearch />
                 </Link>
+
                 <input type="text" autoFocus className={search.input} ref={inputRef} placeholder="Search your next course" onChange={e => setQuery(e.target.value)} />
                 <span className={search.cross}><RxCross2 onClick={clear} className={search.crossSvg} /></span>
-                <button className={search.button}>
-                    <Link className={search.a} href={`/details/${query.split(' ').join('-')}`}>
+                <input type='submit' className={search.button}>
+                    {/* <Link className={search.a} href={`/details/${query.split(' ').join('-')}`}>
                         Search
-                    </Link>
-                </button>
-            </div>
+                    </Link> */}
+                </input>
+
+            </form>
         </div>
     )
 }
