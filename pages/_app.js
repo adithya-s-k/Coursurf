341f8f7bc7f550b02797a054c4ff23411ce3d464
Adithya S K <adithyaskolavi@gmail.com>
2023-03-05 17:34:26 +0530

    fixed the font issue - Adithya SK

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,20 @@
 import '../styles/globals.css';
-import '../sass/styles.scss'
+import '../sass/styles.scss';
+
 import { ChakraProvider } from '@chakra-ui/react';
 import Navbar from '../components/navbar';
 import Footer from '../components/footer';
 import { AnimatePresence } from 'framer-motion';
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <>
+      <ChakraProvider>
         <AnimatePresence mode="wait" initial={false} />
         <Navbar />
         <Component {...pageProps} />
         <Footer />
-    </ChakraProvider>
+      </ChakraProvider>
+    </>
   );
 }
 
